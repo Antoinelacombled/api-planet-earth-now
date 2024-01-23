0def4db1ce859c0bf315a84634f9d52767219b47
Antoinelacombled <antoine.j.lacombled@gmail.com>
2024-01-23 22:31:33 +0100

    feat: config heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("https://api-planet-earth-now-98409e4028dd.herokuapp.com"),
+  url: "https://api-planet-earth-now-98409e4028dd.herokuapp.com",
   proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
