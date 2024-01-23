ca8bdc1d4d5732874aeee6e444b18b83edb1b0a0
Antoinelacombled <antoine.j.lacombled@gmail.com>
2024-01-23 22:37:13 +0100

    feat: config heroku

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,14 @@
 module.exports = ({ env }) => ({
   url: "https://api-planet-earth-now-98409e4028dd.herokuapp.com",
   proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
   },
 });
